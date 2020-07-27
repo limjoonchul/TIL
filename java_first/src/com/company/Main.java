856ebf678cd1cafbed38a69ff7d925916395c7dd
LIMJOONCHUL <dlawnscjf511@naver.com>
2020-07-27 16:48:08 +0900

    자바 처음

---
+++ java_first/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.time.Clock;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+//        sout 자동완성
+    }
+}
